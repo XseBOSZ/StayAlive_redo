17db55a48619849520014dc2038ef8fa18119bbc
XseBOSZ <kacper.m1290@gmail.com>
2022-11-09 23:25:53 +0100

    TMP + Console

--- Assets/Scripts/Debug/Console/Console.cs
+++ Assets/Scripts/Debug/Console/Console.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Console : MonoBehaviour
 {
-    public void SendCommand(string command)
+    public TMP_InputField Input;
+
+    public void SendCommand()
     {
+        string Command = Input.text.ToString();
+        var args = Command.Split(";");
+        
+    }
+
+    void SplitCommand(string arg)
+    {
+        var function = arg.Split(" ");
+
+
+    }
+
+    void TranslateCommand(string functionWithValue)
+    {
+        if(functionWithValue[0].Equals("LoadScene"))
+        {
+            SceneManager.LoadScene(functionWithValue[1]);
+        }
+
+
 
     }
 }
