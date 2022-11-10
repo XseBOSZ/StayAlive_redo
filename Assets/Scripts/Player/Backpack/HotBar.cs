c61e2bf953c357d53f0fae8b5cc8c6de0a32e31c
XseBOSZ <64862677+XseBOSZ@users.noreply.github.com>
2022-11-10 18:31:16 +0100

    hotbar concept

--- Assets/Scripts/Player/Backpack/HotBar.cs
+++ Assets/Scripts/Player/Backpack/HotBar.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class HotBar : MonoBehaviour
 {
+    public GameObject HotBarObject;
+    [SerializeField]
+    List<GameObject> Slots = new List<GameObject>();
     public List<BPItem> hotbarItems = new List<BPItem>();
 
+    private void Start()
+    {
+        for (int i = 0; i < HotBarObject.transform.childCount; i++) {
+
+            Slots.Add(HotBarObject.transform.GetChild(i).gameObject);
+        }
+    }
     public void UseOnItem()
     {
 
---
+++ Assets/Scripts/Player/Backpack/HotbarSlot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotbarSlot : MonoBehaviour
+{
+    public BPItem itemOnSlot;
+
+    //to not lose the concept - this script is being called by main hotbar script to simply do shit in ScriptableObject's and GameObject's
+    //this will hold actual in game component and reference from backback to just click '1' on numpad and select #1 slot for example
+    //to use/equip. This will become handy as long as i will keep the concept of hotbar 
+
+
+}
