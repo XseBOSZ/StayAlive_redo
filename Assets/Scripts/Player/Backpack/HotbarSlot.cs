a639f5d65f3a9205e7ff69320c66b5df080d89de
XseBOSZ <64862677+XseBOSZ@users.noreply.github.com>
2022-11-11 13:39:24 +0100

    added hotbar slot image manipulation (blank for now), added Template for SlotImage, when i get back to code implemented an IF statement to do other slots visible

--- Assets/Scripts/Player/Backpack/HotbarSlot.cs
+++ Assets/Scripts/Player/Backpack/HotbarSlot.cs
@@ -1,10 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HotbarSlot : MonoBehaviour
 {
     public BPItem itemOnSlot;
+    public Image slotImage;
+    public string resourcesPath;
+    public Sprite SpriteToChange;
+    private void Start()
+    {
+        slotImage = gameObject.GetComponent<Image>();
+        resourcesPath = "UI/Equipment/";
+        UpdateSprite();
+        
+    }
+    
+    void UpdateSprite()
+    {
+        if (itemOnSlot.item == null || itemOnSlot.count == 0)
+        {
+            LoadResource("Slot_Blank");
+            if (SpriteToChange != null) {
+
+               slotImage.sprite = SpriteToChange;
+                //loadedSprite = null;
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+
+        }
+        
+    }
+
+    void LoadResource(string resourceName)
+    {
+        var loadedResource = Resources.Load(resourcesPath+ resourceName) as Texture2D;
+        Rect spriteRect = new Rect(0, 0, loadedResource.width, loadedResource.height);
+        Sprite newSprite = Sprite.Create(loadedResource, spriteRect, new Vector2(0,0));
+        SpriteToChange = newSprite;
+    }
 
     //to not lose the concept - this script is being called by main hotbar script to simply do shit in ScriptableObject's and GameObject's
     //this will hold actual in game component and reference from backback to just click '1' on numpad and select #1 slot for example
